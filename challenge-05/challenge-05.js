ef03ea7e6fc9a115e1dfb56af24d86ded1b66636
Baltazar Parra <baltazar.parra@triata.com.br>
2016-06-16 16:16:50 -0300

    Desafio 5

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,21 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+
+var qualquer = ['nome', 1, (2+2), true ];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+
+function rtrn (arg) {
+  return arg;
+};
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +24,27 @@ segundo, um número. A função deve retornar um índice do array que foi passad
 no primeiro parâmetro. O índice a ser retornado, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+
+function two (arg1, arg2) {
+    return arg1[arg2];
+};
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+
+var numb = [1, 'a', '2', true, 1+1];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+
+> two(numb, 0); // 1
+> two(numb, 1); // 'a'
+> two(numb, 2); // '2'
+> two(numb, 3); // true
+> two(numb, 4); // 2
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,7 +60,33 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+
+function book (x) {
+
+    var livros = {
+        livro1: {
+            quantidadePaginas: 0,
+            autor: 'Eu Mes',
+            editora: 'Babadook'
+        },
+        livro2: {
+            quantidadePaginas: 0,
+            autor: 'Messy',
+            editora: 'Badbad'
+        },
+        livro3: {
+            quantidadePaginas: 0,
+            autor: 'Eu',
+            editora: 'dook'
+        }
+    };
+
+    livroUM = livros.livro1;
+    livroDOIS = livros.livro2;
+    livroTRES = livros.livro3;
+
+    return (x === undefined ? livros : x);
+};
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
