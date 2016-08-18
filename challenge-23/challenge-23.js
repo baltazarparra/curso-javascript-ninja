ed82ebf74774307c44412dea0c5b88ef748c7123
baltazarparra <baltazarparra@outlook.com>
2016-08-18 11:36:30 -0300

    23

--- challenge-23/challenge-23.js
+++ challenge-23/challenge-23.js
@@ -2,32 +2,6 @@
 
     'use strict';
 
-    /*
-    Vamos desenvolver mais um projeto. A ideia é fazer uma mini-calculadora.
-    As regras são:
-
-    - Deve ter somente 1 input, mas não deve ser possível entrar dados nesse input
-    diretamente;
-    - O input deve iniciar com valor zero;
-    - Deve haver 10 botões para os números de 0 a 9. Cada botão deve ser um número;
-    - Deve haver 4 botões para as operações principais: soma (+), subtração(-),
-    multiplicação(x) e divisão(÷);
-    - Deve haver um botão de "igual" (=) que irá calcular os valores e um botão "CE"
-    que irá limpar o input, deixando-o com valor 0;
-
-    - A cada número pressionado, o input deve atualizar concatenando cada valor
-    digitado, como em uma calculadora real;
-    - Ao pressionar um botão com uma das 4 operações, deve aparecer o símbolo da
-    operação no input. Se o último caractere no input já for um símbolo de alguma
-    operação, esse caractere deve ser substituído pelo último pressionado.
-    Exemplo:
-    - Se o input tem os valores: "1+2+", e for pressionado o botão de
-    multiplicação (x), então no input deve aparecer "1+2x".
-    - Ao pressionar o botão de igual, o resultado do cálculo deve ser mostrado no
-    input;
-    - Ao pressionar o botão "CE", o input deve ficar zerado.
-    */
-
     var $input = document.querySelector('[data-js="input"]');
     var $button = document.querySelectorAll('[data-js="button"]');
     var $ce = document.querySelector('[data-js="ce"]');
@@ -50,7 +24,7 @@
     }
 
     function handleOpe(e) {
-        remove();
+        $input.value = remove($input.value);
         $input.value += this.value;
     }
 
@@ -58,21 +32,39 @@
         $input.value = 0;
     }
 
-    function isLast() {
+    function isLast(number) {
         var opera = ['+', '-', '*', '/'];
-        var lastItem = $input.value.split('').pop();
+        var lastItem = number.split('').pop();
         return opera.some(function(operator) {
             return operator === lastItem;
         });
     }
 
-    function remove() {
-        if(isLast())
-            $input.value = $input.value.slice(0, -1);
+    function remove(number) {
+        if(isLast(number)) {
+            return number.slice(0, -1);
+        }
+        return number;
     }
 
     function handleEqual(e) {
-        remove();
+        $input.value = remove($input.value);
+        var allValues = $input.value.match(/\d+[+*/-]?/g);
+        $input.value = allValues.reduce(function(accumulated, actual) {
+            var firstValue = accumulated.slice(0, -1);
+            var operator = accumulated.split('').pop();
+            var lastValue = actual;
+            switch(operator) {
+                case '+':
+                    return Number(firstValue) + Number(lastValue);
+                case '-':
+                    return Number(firstValue) - Number(lastValue);
+                case '*':
+                    return Number(firstValue) * Number(lastValue);
+                case '/':
+                    return Number(firstValue) / Number(lastValue);
+            }
+        });
     }
 
 })(window, document);
